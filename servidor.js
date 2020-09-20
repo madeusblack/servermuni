c14b4d07d2b87f8b3ce0c354c3fcb62b92cd078d
Yayson Olivares <yayson.olivares@gmail.com>
2020-09-19 23:27:09 -0300

    prepared files for http2 support
    prepared files for http2 support
    and added validation for zerossl site

--- servidor.js
+++ servidor.js
@@ -1,13 +1,26 @@
 const express = require('express');
 const path = require('path');
+const spdy = require('spdy');
+const fs = require('fs');
+const http = require('http');
 const app = express();
+const CERTS_ROOT = './';
+
 app.use(express.static(path.join(__dirname, 'build')));
 app.get('/', function(req, res) {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 app.use('/transp', express.static('transp'));
+app.use('/.well-known', express.static('well'));
+
 app.get('*', function(req, res){
   res.redirect('/');
 });
-
-app.listen(8080,()=>console.log('Servidor Funcionando en el puerto 80'));
+const credentials = {
+  cert: fs.readFileSync(path.resolve(CERTS_ROOT, 'muninogales.cert')),
+  key: fs.readFileSync(path.resolve(CERTS_ROOT, 'muninogales.key')),
+};
+const httpServer = http.createServer(app);
+const httpsServer = spdy.createServer(credentials, app);
+httpServer.listen(8080);
+httpsServer.listen(8430);
