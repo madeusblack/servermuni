bf4a8186dac1b9d174f51bf49318330369a5803b
Yayson Olivares <yayson.olivares@gmail.com>
2021-03-04 10:30:27 -0300

    updated status of covid
    updated status of covid

--- src/containers/PasoAPasoContainer.jsx
+++ src/containers/PasoAPasoContainer.jsx
@@ -1,63 +1,46 @@
+/* eslint-disable react/jsx-one-expression-per-line */
 import React from 'react';
 import '../assets/styles/PasoAPaso.scss';
 
 const PasoAPasoContainer = () => (
 
   <div className='pasoAPasoContainer'>
-    <h1 className='mainTitleCovid'>Que significa el Paso 3 En Preparación?</h1>
+    <h1 className='mainTitleCovid'>¿Qué está prohibido en mi comuna?</h1>
+    <p>
+      Las reuniones en residencias particulares de más de 5 personas y cualquier evento o reunión durante el horario de toque de queda, o los sábados, domingos y festivos.<br />
+      Las actividades con público con más asistentes de lo permitido o sin cumplir las condiciones requeridas según el tipo.<br />
+      La atención de público en restaurantes o cafés en espacios cerrados.<br />
+      El consumo de alimentos en lugares cerrados de centros comerciales.<br />
+      El funcionamiento de Clubes de Adultos Mayores y Centros de día de Adultos Mayores.<br />
+      El funcionamiento de pubs, discotecas y lugares análogos.<br />
+      El traslado a residencia no habitual y a otras regiones, excepto en posesión del Permiso (único) de Vacaciones.<br />
+    </p>
     <h2 className='mainTitleCovid'>¿Qué puedo hacer?</h2>
     <p>
-      Puedes salir todos los días de la semana, pero no puedes ir a un lugar en cuarentena.
-      <br />
-      Debes respetar el toque de queda, que implica que no puedes salir entre las 22:00 y la 05:00 horas salvo que tengas un salvoconducto por emergencia médica o el fallecimiento de un familiar directo.
-      <br />
-      Puedes ir a trabajar si es que trabajas en un lugar que no esté en cuarentena, a no ser que sea servicio esencial, y tengas un permiso de desplazamiento colectivo o un permiso único colectivo.
-      <br />
-      Participar en eventos, reuniones sociales y recreativas de máximo 50 personas.
-      <br />
-      Realizar actividad deportiva y participar en deportes colectivos con máximo 5 personas en lugares cerrados (excepto gimnasios de acceso público) y 25 en lugares abiertos. Sin
-      público.
-      <br />
-      Si eres mayor de 75 años, podrás salir a pasear una vez al día, todos los días, por 60 minutos, entre las 10:00 y las 12:00 horas o entre las 15:00 y las 17:00 horas.
-      <br />
-      Cada vez que salgas, debes usar mascarilla, mantener la distancia física y recordar medidas como lavado de manos o uso de alcohol gel.
-      <br />
+      Todos, incluidos los mayores de 75 años, pueden salir, pero no ir a lugares en cuarentena.<br />
+      Debes quedarte en casa los fines de semana y los feriados, días en que solo podrás salir con un permiso individual que puedes obtener en comisariavirtual.cl (un permiso a la semana).<br />
+      Debes respetar el toque de queda, que implica que no puedes salir entre las 23:00 y las 05:00 horas salvo que tengas un salvoconducto por emergencia médica o funeral.<br />
+      Puedes ir a trabajar si es que trabajas en un lugar que no esté en cuarentena, a no ser que sea servicio esencial, y tengas un permiso de desplazamiento colectivo o un permiso único colectivo.<br />
+      Asistir a actividades presenciales de educación: salas cuna, educación parvularia, básica y media, pero solo de lunes a viernes.<br />
+      Asistir a restaurantes, sólo en terrazas o espacios al aire libre y en grupos de máximo 4 personas.<br />
+      Realizar actividad deportiva. De lunes a viernes, en espacios abiertos, en grupos de máximo 10 personas. En espacios cerrados y gimnasios, asistir por cohortes, y mantener siempre la distancia de 2 m. entre personas. No más de 5 personas por sala de clases y cumpliendo 1 persona cada 8m2 en salas de máquinas. Prefiere espacios abiertos. Los fines de semana y festivos, sólo en la banda horaria de 07 a 08:30 hrs., y sin usar auto o transporte público.<br />
+      Tener reuniones sociales en residencias particulares o lugares públicos abiertos, con máximo 5 personas en total, y sólo de lunes a viernes.<br />
+      Intenta no tener reuniones con más de un mismo grupo a la semana.<br />
+      Participar en eventos o actividades con público: si la actividad es con ubicación fija de los asistentes y sin consumo de alimentos, pueden ser hasta 20 personas en lugares al aire libre y 10 personas en lugares cerrados, sólo de lunes a viernes. Si la misma actividad considera consumo de alimentos, sólo pueden ser 10 personas al aire libre. Estas actividades no pueden ser en residencias particulares.<br />
+      Residentes de Centros SENAME pueden salir hasta 3 veces a la semana, con autorización y supervisión. También se permiten visitas de vínculos significativos. Los fines de semana pueden salir a realizar actividades al aire libre en la banda horaria de 07:00 a 08:30 horas, con autorización y supervisión.<br />
+      Residentes de ELEAM pueden salir a caminar todos los días por máximo 2 horas, y pueden recibir visita de 2 personas, 2 veces a la semana.<br />
+      Puedes salir de vacaciones con el Permiso de Vacaciones, dispuesto en Comisaría Virtual. Se puede obtener una única vez por cada persona y estará disponible hasta el 31 de marzo para ir a comunas en Paso 2, 3 o 4, teniendo que permanecer en el mismo destino durante toda la estadía.<br />
+      Cada vez que salgas, con la autorización correspondiente, debes usar mascarilla, mantener la distancia física y recordar medidas como lavado de manos o uso de alcohol gel.<br />
     </p>
     <h2 className='mainTitleCovid'>¿Qué puede funcionar en mi comuna?</h2>
     <p>
-      Farmacias, supermercados y otros lugares de abastecimiento de alimentos y productos básicos, centros de salud y establecimientos de servicio público.
-      <br />
-      Comercio y otras actividades no esenciales que puedan funcionar con trabajadores que vivan en una comuna sin cuarentena.
-      <br />
-      Todo lo que funcione, debe ser siguiendo los protocolos establecidos
-      <br />
-    </p>
-
-    <h2 className='mainTitleCovid'>¿Qué está prohibido en mi comuna? </h2>
-    <p>
-      El funcionamiento de Clubes y Centros de día de Adulto Mayores.
-      <br />
-      El funcionamiento de cines, teatros, pubs, discotecas y lugares análogos.
-      <br />
-      La atención de público en restaurantes y cafés.
-      <br />
-      El funcionamiento de gimnasios abiertos al público.
-      <br />
-      La realización de actividad deportiva con más de 25 personas en lugares abiertos y más de 5 en lugares cerrados.
-      <br />
-      La realización y participación de eventos y reuniones sociales y recreativas de más de 50
-      personas.
-      <br />
-      La entrada y salida a Centros SENAME y ELEAM.
-      <br />
-      El traslado a segunda vivienda.
-      <br />
-      El libre tránsito por fronteras terrestres y áreas, a no ser que la autoridad competente
-      indique lo contrario.
-      <br />
-      La realización de cirugías electivas no críticas, exceptuando cirugía mayor ambulatoria y con hospitalización de máximo una noche, a no ser que la autoridad competente indique lo contrario.
-      <br />
-      Las clases presenciales de establecimientos educacionales, salvo situaciones excepcionales, previa solicitud del alcalde y cumpliendo las condiciones sanitarias.
+      Farmacias, supermercados y otros lugares de abastecimiento de alimentos y productos básicos, centros de salud y establecimientos de servicio público. El fin de semana y festivos sólo con venta presencial de productos esenciales.<br />
+      Comercio y otras actividades no esenciales que puedan funcionar con trabajadores que vivan en una comuna sin cuarentena.<br />
+      Actividades presenciales en salas cuna y establecimientos de educación parvularia, básica y media, de lunes a viernes.<br />
+      Atención de público en restaurantes sólo en espacios abiertos, con 2m. lineales entre mesas (bordes).<br />
+      Los lugares deportivos cerrados y gimnasios, con ventilación permanente. Los asistentes deben asistir por cohortes que no pueden interactuar entre sí. No puede haber más de 5 personas en una misma sala, y en la sala de máquinas se debe cumplir una persona cada 8m2. Siempre mantener 2 metros de distancia.<br />
+      Actividades con público, respetando los aforos máximos y las condiciones de acuerdo al tipo de evento.<br />
+      Todo lo que funcione, debe ser siguiendo los protocolos establecidos.<br />
     </p>
     <div className='covidContainer'>
       <h2>Modo Covid Estrategia Sanitaria Permanente</h2>
@@ -65,9 +48,10 @@ const PasoAPasoContainer = () => (
         Aislamiento obligatorio para: casos confirmados, sospechosos y probables.
         <br />
         Cuarentena obligatoria para contactos estrechos de casos confirmados
+        <br />
         Cuarentena obligatoria para quienes ingresan al país.
         <br />
-        Uso de mascarilla en lugares cerradosy abiertos urbanos, y transporte público y privado.
+        Uso de mascarilla en lugares cerrados y abiertos urbanos, y transporte público y privado.
         <br />
         Mantener en todo momento un distanciamiento físico mínimo de un metro entre sí, excepción medios de transporte y lugares con aforo regulado específicamente.
         <br />
