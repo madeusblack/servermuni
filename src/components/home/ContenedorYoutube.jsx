36b3a5590663ebe57c2e567c9c48bfcc02335c19
Yayson Olivares <yayson.olivares@gmail.com>
2021-03-29 14:16:26 -0300

    fixed yt video title

--- src/components/home/ContenedorYoutube.jsx
+++ src/components/home/ContenedorYoutube.jsx
@@ -6,7 +6,11 @@ const ContenedorYoutube = () => {
   return (
     <div className='mainContainerYoutube'>
       <div className='contenedorIframes'>
-        <VideoItem link='https://www.youtube.com/watch?v=-Cr4oazxWJI&list=PLNXnxTQw1v0ZYPdhisJHiQfQgCfe59HVE' fileName='youtube' title='Escuelas Abiertas' />
+        <VideoItem
+          link='https://www.youtube.com/watch?v=-Cr4oazxWJI&list=PLNXnxTQw1v0ZYPdhisJHiQfQgCfe59HVE'
+          fileName='youtube'
+          title='Día Internacional de la Mujer'
+        />
       </div>
     </div>
   );
