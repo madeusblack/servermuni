b3fabaffa0e99603be5b69db2855254d9644d044
Yayson Olivares <yayson.olivares@gmail.com>
2021-06-15 13:54:57 -0400

    updated data

--- src/components/home/ClasesOnline.jsx
+++ src/components/home/ClasesOnline.jsx
@@ -2,6 +2,9 @@ import React from 'react';
 import styles from '../../assets/styles/CovidVacunacion.module.css';
 import clasesOnline from '../../assets/static/Covid/remate.jpg';
 import clasesOnlinewebp from '../../assets/static/Covid/remate.webp';
+import ordenanzapdf from '../../assets/static/pdf/ordenanza.pdf';
+import ordenanza from '../../assets/static/header/ordenanza.png';
+import ordenanzawebp from '../../assets/static/header/ordenanza.webp';
 
 const ClasesOnline = () => (
   <div className={styles.container}>
@@ -11,6 +14,16 @@ const ClasesOnline = () => (
       <source srcSet={clasesOnline} type='image/jpeg' />
       <img className={styles.image} csr={clasesOnline} alt='' />
     </picture>
+
+    <h5 className={styles.titleWide1}>ORDENANZA MUNICIPAL SOBRE CARRERAS CLANDESTINAS Y RUIDOS MOLESTOS POR BRAMADORES O ESCAPES LIBRE DE VEHÍCULOS</h5>
+    <a href={ordenanzapdf}>
+      <picture>
+        <source srcSet={ordenanzawebp} type='image/webp' />
+        <source srcSet={ordenanza} type='image/png' />
+        <img className={styles.image} csr={ordenanza} alt='' />
+      </picture>
+    </a>
   </div>
+
 );
 export default ClasesOnline;
