3bc8928802ea7b5a44e67d2c18ba215233a5d840
Yayson Olivares <kazeyaz@gmail.com>
2021-09-29 13:06:26 -0300

    actually really removed escalafon

--- src/components/municipalidad/EscalafonBody.jsx
+++ src/components/municipalidad/EscalafonBody.jsx
@@ -1,18 +1,19 @@
 import React from 'react';
 import escalafon from '../../assets/static/pdf/municipalidad/escalafon.pdf';
-//import pdfImg from '../../assets/static/PDF.svg';
+import pdfImg from '../../assets/static/PDF.svg';
 import styles from '../../assets/styles/LayoutContenidoMuniComuna.module.css';
-import Organigrama from './Organigrama';
-//<img className={styles.pdfImg} src={pdfImg} alt='' />
+//import Organigrama from './Organigrama';
+//<Organigrama />
 
 const EscalafonBody = () => (
   <div className={styles.escalafonContainer}>
     <h1 className={styles.muniTitle}>ORGANIGRAMA MUNICIPAL</h1>
     <div />
+
     <a className={styles.escalafonPdf} href={escalafon}>
+      <img className={styles.pdfImg} src={pdfImg} alt='' />
       Escalafón Municipal
     </a>
-    <Organigrama />
   </div>
 );
 export default EscalafonBody;
