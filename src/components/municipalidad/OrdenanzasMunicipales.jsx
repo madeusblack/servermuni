b162ba11743a7bd375773bfc1af4a630cc7b4f17
Yayson Olivares Herrera <yolivares@zolbit.com>
2021-12-28 12:11:42 -0300

    added ordenanza

--- src/components/municipalidad/OrdenanzasMunicipales.jsx
+++ src/components/municipalidad/OrdenanzasMunicipales.jsx
@@ -4,13 +4,17 @@ import decreto1378 from '../../assets/static/ordenanzas/DECRETO 1378.pdf';
 import decreto717 from '../../assets/static/ordenanzas/publicacion diario oficial 12 junio 2021 bramadores y carreras clandestinas.pdf';
 import decreto1568 from '../../assets/static/ordenanzas/DECRETO 1568 PROHIBICIÓN DE INGRESO DE VENDEDORES EN DEPENDENCIAS MUNICIPALES.pdf';
 import decreto1550 from '../../assets/static/ordenanzas/APRUEBA AGREGAR ART.5º TER A ORDENANZA DERECHOS MUNICIPALES.pdf';
+import decreto2496 from '../../assets/static/ordenanzas/DECRETO ALCALDICIO 2496 DEROGACION ARTICULO ORDENANZA ANIMALES.pdf';
 import styles from '../../assets/styles/LayoutContenidoMuniComuna.module.css';
 
 const OrdenanzasMunicipales = () => (
   <div className={styles.muniContainer}>
     <h1 className={styles.muniTitle}>Ordenanzas Municipales</h1>
-
     <div className={styles.ordenanzasContainer}>
+      <a name='decreto1378' href={decreto2496}>
+        <img src={pdfImg} alt='' />
+        DECRETO ALCALDICIO 2496 DEROGACION ARTICULO ORDENANZA ANIMALES
+      </a>
       <a name='decreto1378' href={decreto717}>
         <img src={pdfImg} alt='' />
         ORDENANZA MUNICIPAL SOBRE CARRERAS CLANDESTINAS Y RUIDOS MOLESTOS POR BRAMADORES O ESCAPES LIBRE DE VEHÍCULOS.
